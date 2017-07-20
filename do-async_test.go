782acb740755e0690bc14e2a10398e100ceefb92
Marcus Westin <marcus.westin@gmail.com>
2017-07-20 18:24:03 -0400

    Update examples and README with links to the relevant godoc.org sections

--- do-async_test.go
+++ do-async_test.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"strings"
+	"sync/atomic"
 	"time"
 
 	do "github.com/marcuswestin/go-x-do"
@@ -13,6 +14,66 @@ import (
 var ctx = context.Background()
 
 func Example() {
+	var workCount int32
+	maxParallel := 5
+	ctx := context.Background()
+
+	// Parallel loop:
+	items := make([]string, 50)
+	loopCh := do.ParallelLoop(ctx, len(items), maxParallel,
+		func(i int) error {
+			defer atomic.AddInt32(&workCount, 1)
+			// This function will be running in parallel,
+			// with at most 5 concurrent executions.
+			// fmt.Println("Item", i, items[i])
+			return nil
+		})
+
+	// Parallel channel read:
+	ch := make(chan interface{})
+	go func() {
+		for i := 0; i < 50; i++ {
+			line := fmt.Sprint("String ", i)
+			ch <- line
+		}
+		close(ch)
+	}()
+	readCh := do.ParallelRead(ctx, ch, maxParallel,
+		func(workItem interface{}) error {
+			defer atomic.AddInt32(&workCount, 1)
+			// line := workItem.(string)
+			// fmt.Println("Do work on line: ", line)
+			return nil
+		})
+
+	// Parallel work generation and execution:
+	workCh := do.ParallelWork(ctx, maxParallel,
+		func(workCh chan<- interface{}) error {
+			for i := 0; i < 50; i++ {
+				line := fmt.Sprint("String ", i)
+				workCh <- line
+			}
+			return nil
+		},
+		func(workItem interface{}) error {
+			defer atomic.AddInt32(&workCount, 1)
+			// line := workItem.(string)
+			// fmt.Println("Do work on line: ", line)
+			return nil
+		})
+
+	// Wait for multiple error channels. Bails early if any of them has an error.
+	err := do.WaitForErrorChannels(ctx, loopCh, readCh, workCh)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	fmt.Println("Done work:", workCount)
+
+	// Output: Done work: 150
+}
+
+func ExampleWaitForErrorChannels() {
 	errCh1 := do.Async(func() error {
 		time.Sleep(1 * time.Millisecond)
 		fmt.Println("Hello 1")
--- do.go
+++ do.go
@@ -1,58 +1,11 @@
-package do
-
 // Package do gives you powerful asyncronous and parallel execution tooling.
 //
-// Example parallel execution usage:
+// For API reference, see https://godoc.org/github.com/marcuswestin/go-x-do#pkg-index.
 //
-// 	maxParallel := 5
-// 	ctx := context.Background()
+// For general example usage, see https://godoc.org/github.com/marcuswestin/go-x-do#example-package.
 //
-// 	// Parallel loop:
-// 	items := make([]string, 50)
-// 	loopCh := do.ParallelLoop(ctx, len(items), maxParallel,
-// 	    func(i int) error {
-// 	        // This function will be running in parallel,
-// 	        // with at most 5 concurrent executions.
-// 	        log.Println("Item", i, items[i])
-// 	        return nil
-// 	    })
-//
-// 	// Parallel channel read:
-// 	ch := make(chan string)
-// 	go func() {
-// 	    for i := 0; i<50; i++ {
-// 	        line := fmt.Sprint("String ", i)
-// 	        ch <- line
-// 	    }
-// 	    close(ch)
-// 	}()
-// 	readCh := do.ParallelRead(ctx, ch, maxParallel,
-// 	    func(workItem interface{}) error {
-// 	        line := workItem.(string)
-// 	        log.Println("Do work on line: ", line)
-// 	        return nil
-// 	    })
-//
-// 	// Parallel work generation and execution:
-// 	workCh := do.ParallelWork(ctx, numParallel,
-// 	    func(workCh chan<- interface{}) error {
-// 	        for i := 0; i<50; i++ {
-// 	            line := fmt.Sprint("String ", i)
-// 	            workCh <- line
-// 	        }
-// 	        return nil
-// 	    },
-// 	    func(workItem interface{}) error {
-// 	        line := workItem.(string)
-// 	        log.Println("Do work on line: ", line)
-// 	        return nil
-// 	    })
-//
-// 	// Wait for multiple error channels. Bails early if any of them has an error.
-// 	err := do.WaitForErrorChannels(loopCh, readCh, workCh)
-// 	if err != nil {
-// 	    log.Println("Error: ", err)
-// 	}
+// For specific function examples and tests, see https://godoc.org/github.com/marcuswestin/go-x-do#pkg-examples.
+package do
 
 import (
 	"context"
